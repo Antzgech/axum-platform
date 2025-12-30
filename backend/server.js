5a01b1f5191b4f696247c5ed0a1b41ae93ad7a2a
Antzgech <engidaworkanteneh@gmail.com>
2025-12-30 04:28:34 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
         "https://web.telegram.org",  
     "https://telegram.org",         
     "https://t.me"                 
